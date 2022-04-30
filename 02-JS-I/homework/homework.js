0272e9f5340299ad2cf0a1b4b49c7fe43d3dea9c
Facundo Dominguez <facud7@gmail.com>
2022-04-30 15:17:20 -0300

    commit first change

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -18,6 +18,8 @@ const nuevaMultiplicacion = 10 * null === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
 
+const stringTestGit = null;
+
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
